cd0d9f73b60f28bcdc14d45b570b03fe497b99e3
Erik Gúth <guthember@gmail.com>
2017-01-11 13:17:17 +0100

    Megoldva

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Fájlműveletek
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        // Fájl beolvasás
+        FileStream file = new FileStream("adat.txt", FileMode.Open);
+        StreamReader fileBe = new StreamReader(file);
+        int szam;
+        int osszeg = 0;
+
+
+        while (!fileBe.EndOfStream)
+        {
+            szam = Convert.ToInt32(fileBe.ReadLine());
+            osszeg += szam;
+        }
+
+        Console.WriteLine("A számok összege={0}",osszeg);
+
+        fileBe.Close();
+        file.Close();
+        Console.ReadKey();
+    }
+}
+
